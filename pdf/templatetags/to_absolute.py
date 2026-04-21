3a562f0d9ed0bb85bc341d39456419833d6e7e39
dgreen <dgreen@library.ohio.gov>
2026-04-21 11:20:52 -0400

    ADD: Enhance PDF layout with improved table structure and accessibility features

--- pdf/templatetags/to_absolute.py
+++ pdf/templatetags/to_absolute.py
@@ -1,6 +1,8 @@
-from django import template
+from pathlib import Path
 
-from cobaa.settings.base import STATIC_ROOT
+from django import template
+from django.conf import settings
+from django.contrib.staticfiles import finders
 
 register = template.Library()
 
@@ -8,5 +10,11 @@ register = template.Library()
 # See: https://github.com/xhtml2pdf/xhtml2pdf/issues/388
 
 @register.filter
-def to_absolute(img):
-    return str(STATIC_ROOT) + img
+def to_absolute(asset_path):
+    normalized_path = str(asset_path).lstrip("/")
+    located = finders.find(normalized_path)
+    if isinstance(located, (list, tuple)):
+        located = located[0] if located else None
+    if located:
+        return located
+    return str(Path(settings.STATIC_ROOT) / normalized_path)
